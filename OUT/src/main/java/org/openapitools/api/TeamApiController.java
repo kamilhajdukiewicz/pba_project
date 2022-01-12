2efbb1c618807abb9b7e51cd05c4fd01cd6471f3
Kamil <kamhaj97@gmail.com>
2022-01-12 13:58:50 +0100

    Added empty implementation for TeamApiController

--- OUT/src/main/java/org/openapitools/api/TeamApiController.java
+++ OUT/src/main/java/org/openapitools/api/TeamApiController.java
@@ -1,6 +1,13 @@
 package org.openapitools.api;
 
+import io.swagger.annotations.ApiParam;
+import org.openapitools.model.Player;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 import java.util.Optional;
@@ -19,6 +26,12 @@ public class TeamApiController implements TeamApi {
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
+    }
+
+    @Override
+    public ResponseEntity<Player> getTeamTeamID(@ApiParam(value = "Id of the team.", required = true) @PathVariable("teamID") String teamID, @ApiParam(value = "data-time of request") @RequestHeader(value = "Data-time", required = false) String dataTime, @ApiParam(value = "id of request") @RequestHeader(value = "Id", required = false) String id) {
+
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
 }
