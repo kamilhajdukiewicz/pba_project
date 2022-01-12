65a72a6fff45945abd8622087a8d5db8ee1d4f6f
Kamil <kamhaj97@gmail.com>
2022-01-12 14:34:55 +0100

    Update Player repo

--- OUT/src/main/java/org/openapitools/repository/PlayersRepository.java
+++ OUT/src/main/java/org/openapitools/repository/PlayersRepository.java
@@ -4,10 +4,37 @@ import lombok.Getter;
 import org.openapitools.model.PlayerDB;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Getter
 public class PlayersRepository {
     private List<PlayerDB> listOfPlayers;
+
+    public PlayersRepository() {
+        this.listOfPlayers = new ArrayList<PlayerDB>();
+    }
+
+    public PlayersRepository(List<PlayerDB> listOfUsers) {
+        this.listOfPlayers = listOfUsers;
+    }
+
+    public void addNewPlayer(PlayerDB newUser) {
+        this.listOfPlayers.add(newUser);
+    }
+
+    public boolean deletePlayerById(UUID id) {
+        return this.listOfPlayers.removeIf(player -> player.getId().equals(id));
+    }
+
+    public boolean updatePlayerById(UUID id, PlayerDB updatedUser) {
+        PlayerDB usr = listOfPlayers.stream().filter(user -> id.equals(user.getId())).findFirst().orElse(null);
+        if(usr != null) {
+            this.listOfPlayers.set(this.listOfPlayers.indexOf(usr), updatedUser);
+            return true;
+        }
+        return false;
+    }
 }
