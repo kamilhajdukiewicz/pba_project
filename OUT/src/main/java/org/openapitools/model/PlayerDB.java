ae5d1514fdf319ea1c03783433f8a5110a7d168b
Kamil <kamhaj97@gmail.com>
2022-01-12 14:25:10 +0100

    Added DB versions of Player and Team

--- OUT/src/main/java/org/openapitools/model/PlayerDB.java
+++ OUT/src/main/java/org/openapitools/model/PlayerDB.java
@@ -1,4 +1,38 @@
 package org.openapitools.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlayerDB {
+
+    private UUID id = null;
+
+    private String firstName = null;
+
+    private String lastName = null;
+
+    private Integer age = null;
+
+    private Integer height = null;
+
+    private String nationality = null;
+
+    private Position position = null;
+
+    private Integer goalsCount = null;
+
+    private Integer assistCount = null;
+
+    private Integer yellowCardCount = null;
+
+    private Integer redCardCount = null;
+
+    private TeamDB team = null;
 }
--- OUT/src/main/java/org/openapitools/model/TeamDB.java
+++ OUT/src/main/java/org/openapitools/model/TeamDB.java
@@ -1,4 +1,16 @@
 package org.openapitools.model;
 
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamDB {
+    private UUID id = null;
+
+    private String name = null;
 }
